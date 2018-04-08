4027c49240abae654809d7cbc996b8a4681c23e5
Ayesha Mutiara <a100017@aup.edu>
2018-04-08 20:58:58 +0000

    can successfully add new teaching activity w/ confirmation page

--- app/controllers/teaching_activities_controller.rb
+++ app/controllers/teaching_activities_controller.rb
@@ -1,4 +1,41 @@
 class TeachingActivitiesController < ApplicationController
+    before_action :find_tactivity, only: [:show, :edit, :update, :destroy]
+    
     def index
     end
+    
+    def show
+    end
+    
+    def new
+        @teaching_activity = TeachingActivity.new
+    end
+    
+    def create
+        @teaching_activity = TeachingActivity.new(tactivity_params)
+        if @teaching_activity.save
+            redirect_to @teaching_activity
+        else 
+            render 'new'
+        end 
+    end
+    
+    def edit
+    end
+    
+    def update
+    end 
+    
+    def destroy
+    end 
+    
+    private
+    
+    def tactivity_params
+        params.require(:teaching_activity).permit(:date, :name, :ta1, :ta2, :ta3, :ta4, :status)
+    end 
+    
+    def find_tactivity
+        @teaching_activity = TeachingActivity.find(params[:id])
+    end 
 end
